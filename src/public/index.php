fa02882904eab3d6619cb3590be05a82c3292b39
Eugenio <eugeniojhe@gmail.com>
2024-09-10 09:56:57 -0300

    CRM PHP  - Ajustando o index
    -Index ajustado, pelo relacionamento do uso das bibliotecas de bootstrap
    via CDN

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__.DIRECTORY_SEPARATOR.'initialize.php';
 echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
+echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">';
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>';
+echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>';
+
 
 initializeClassLoader();
 
